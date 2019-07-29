fcbb77acf186fac1d1bab5ba62241770174a4ae9
typhain <typhaine.charrat@gmail.com>
2019-07-29 14:33:44 +0200

    all_gossips update

--- lib/controller.rb
+++ lib/controller.rb
@@ -10,16 +10,21 @@ class ApplicationController < Sinatra::Base
     erb :new_gossip
   end
 
+  post '/gossips/new/' do
+  Gossip.new(params["gossip_author"], params["gossip_content"]).save
+  redirect '/'
+  end
+
   # post '/gossips/new/' do
-  # Gossip.new("super_auteur", "super gossip").save
+  # puts "Salut, je suis dans le serveur"
+  # puts "Ceci est le contenu du hash params : #{params}"
+  # puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
+  # puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
+  # puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
   # end
 
-  post '/gossips/new/' do
-  puts "Salut, je suis dans le serveur"
-  puts "Ceci est le contenu du hash params : #{params}"
-  puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
-  puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
-  puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+  get '/all_gossips/' do
+  erb :all_gossips, locals: {gossips: Gossip.all}
   end
 
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -15,4 +15,12 @@ class Gossip
     end
   end
 
+  def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+
 end
